f9667fe4c37b16185dbdbcdb1437779681427a3b
Melnyk Mykhailo <m.melnyk.ia12@kpi.ua>
2024-01-29 21:48:12 +0200

    fix code style in heading navbar and sharelinkcomponent

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,22 +1,24 @@
 import Link from "next/link";
 
 export default function NavBar() {
-    return (
-        <nav>
-            <ul className='flex gap-2'>
-                <li>
-                    <Link href="/"
-                        className="font-bold font-orbitron text-orange-800 hover:underline">
-                        Indie Gamer
-                    </Link>
-                </li>
-                <li className="ml-auto">
-                    <Link href="/reviews">Reviews</Link>
-                </li>
-                <li>
-                    <Link href="/about">About</Link>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav>
+      <ul className="flex gap-2">
+        <li>
+          <Link
+            href="/"
+            className="font-bold font-orbitron text-orange-800 hover:underline"
+          >
+            Indie Gamer
+          </Link>
+        </li>
+        <li className="ml-auto">
+          <Link href="/reviews">Reviews</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
--- components/ShareLinkButton.tsx
+++ components/ShareLinkButton.tsx
@@ -1,25 +1,27 @@
-'use client';
+"use client";
 
-import { LinkIcon } from '@heroicons/react/20/solid';
-import { useState } from 'react';
+import { LinkIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
 
 export default function ShareLinkButton() {
-    const [clicked, setClicked] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
-    const handleClick = () => {
-        navigator.clipboard.writeText(window.location.href);
-        setClicked(true);
-        setTimeout(() => setClicked(false), 1500);
-    };
+  const handleClick = () => {
+    navigator.clipboard.writeText(window.location.href);
+    setClicked(true);
+    setTimeout(() => setClicked(false), 1500);
+  };
 
-    console.log('[ShareLinkButton] clicked:', clicked);
-    return (
-        <button onClick={handleClick}
-            className="border flex gap-1 items-center px-2 py-1 rounded
+  console.log("[ShareLinkButton] clicked:", clicked);
+  return (
+    <button
+      onClick={handleClick}
+      className="border flex gap-1 items-center px-2 py-1 rounded
         text-slate-500 text-sm
-        hover:bg-orange-100 hover:text-slate-700">
-            <LinkIcon className="h-4 w-4" />
-            {clicked ? 'Link copied!' : 'Share link'}
-        </button>
-    );
+        hover:bg-orange-100 hover:text-slate-700"
+    >
+      <LinkIcon className="h-4 w-4" />
+      {clicked ? "Link copied!" : "Share link"}
+    </button>
+  );
 }
